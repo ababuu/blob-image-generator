1b20b7eec8a0add254dc6a57b18dae7959ac2cf8
ababu <ababualemu123@gmail.com>
2025-03-17 19:42:17 +0300

    - Update vite.pkg.config.js for build:   - Configure Vite to build the package as a library   - Externalize React and ReactDOM

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: "./src/index.js", // Entry point for the package
+      name: "BlobImageGenerator", // Global variable name for UMD builds
+      fileName: "index", // Output file name
+      formats: ["es", "cjs"], // Output formats (ESM and CommonJS)
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"], // Externalize React and ReactDOM
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+});
